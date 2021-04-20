09acf78ffd8a66008be094f513c3a775eb7ea3b7
Ghibli <aran.t.ali@gmail.com>
2021-04-20 11:17:43 -0400

    schema.sql is ready with 3 tables

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS org_chart;
+CREATE DATABASE org_chart;
+
+USE org_chart;
+
+CREATE TABLE departments(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  dept_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL(10,2),
+  dept_id INTEGER(11),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER(11),
+  manager_id INTEGER(11) NULL,
+  PRIMARY KEY (id)
+);
