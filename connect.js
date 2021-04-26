9fedf05c4a523a765257cedd374e08c56db0b57b
Ghibli <aran.t.ali@gmail.com>
2021-04-26 15:46:02 -0400

    connection file that uses env package is added

---
+++ connect.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const chalk = require('chalk');
+require('dotenv').config();
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // MySQL password from .env
+  password: process.env.CLIENT_PASS,
+  database: 'org_chart',
+});
+
+// connect to the mysql server and sql database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(chalk.green(`Successfully connected to org_chart as id: ${connection.threadId}`));
+});
+
+module.exports = connection;
